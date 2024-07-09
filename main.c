87865328152410d087bd7dcca011e117afdefce3
khomyn06 <kn23-m.khomyn@nubip.edu.ua>
2024-07-09 14:35:17 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+int gcd(int a, int b) { while (b != 0) { int temp = b; b = a % b; a = temp; } return a; }
+
+int lcm(int a, int b) { return (a / gcd(a, b)) * b; }
+
+int main() { int n;
+
+scanf("%d", &n);
+
+int numbers[n];
+
+for (int i = 0; i < n; i++) {
+    scanf("%d", &numbers[i]);
+}
+
+int result = numbers[0];
+for (int i = 1; i < n; i++) {
+    result = lcm(result, numbers[i]);
+}
+
+printf("%d\n", result);
+
+retur
